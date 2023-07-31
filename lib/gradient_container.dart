008d23d0eb6bd5e7529be7373a7d67c93e39ac78
ltpdo <fgh2u58h42xctzxr32x@gmail.com>
2023-08-01 00:01:21 +0900

    first app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,56 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradiendContainer extends StatelessWidget {
+  const GradiendContainer(this.color1, this.color2, {super.key});
+
+  const GradiendContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.deepOrange;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
+
+/*
+class GradiendContainer extends StatelessWidget {
+  const GradiendContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello, World!'),
+      ),
+    );
+  }
+}
+*/
